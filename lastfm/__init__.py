eafc590b710b9af445dedbfec406934edf1ae08a
wdog <wdog666@gmail.com>
2020-11-17 01:04:18 +0100

    lastFM minimal added

---
+++ lastfm/__init__.py
@@ -0,0 +1,67 @@
+#!/bin/env python3
+
+# import sys
+import pylast
+import time
+import datetime
+import json
+from collections import namedtuple
+
+
+class LFM:
+    """last fm connection"""
+    def __init__(self):
+        self.get_settings()
+        self.connect()
+        self.track = None
+        self.artist = None
+
+    def set_track(self, track):
+        self.track = track
+
+    def set_artist(self, artist):
+        self.artist = artist
+
+    # +----------------------------------------+
+    # | Convert Json to Object - hook function |
+    # +----------------------------------------+
+
+    def customSecretsDecoder(self, secretsDict):
+        return namedtuple('X', secretsDict.keys())(*secretsDict.values())
+
+    # +-----------------------------------+
+    # | Read JSON String with credentials |
+    # +-----------------------------------+
+
+    def get_settings(self):
+        with open('secret.js', 'r') as fi:
+            self.settings = json.loads(fi.read(),
+                                       object_hook=self.customSecretsDecoder)
+
+    def connect(self):
+        password_hash = pylast.md5(self.settings.password)
+
+        self.network = pylast.LastFMNetwork(api_key=self.settings.key,
+                                            api_secret=self.settings.secret,
+                                            username=self.settings.username,
+                                            password_hash=password_hash)
+
+    def scrobble(self, artist, track):
+        # Get UNIX timestamp
+        unix_timestamp = int(time.mktime(datetime.datetime.now().timetuple()))
+        self.network.scrobble(artist=artist,
+                              title=track,
+                              timestamp=unix_timestamp)
+
+        # Confirm
+        print("Confirmation from Last.fm:")
+        recent_tracks = self.network.get_user(self.settings.username) \
+                            .get_recent_tracks(limit=1)
+        print(recent_tracks)
+
+    def get_cover(self, artist, album):
+        album = self.network.get_album(artist, album)
+        # wiki test
+        # wiki = album.get_wiki_summary()
+        # print(wiki)
+        return album.get_cover_image(2)
--- songChange.py
+++ songChange.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/bin/env python3
 
 # title          : songChange.py
 # description    : Uses notify send for title artist and album cover
@@ -11,28 +11,68 @@
 
 import os
 import stagger
+import requests
+from lastfm import LFM
+
+
+" TODO "
+" write cover in folder only if not exists "
 
 
 class CmusNotify:
     def __init__(self):
         self.get_filename_path()
         self.get_tags()
+        self.lfm = LFM()
         self.extract_cover()
+
         self.notify(self.tags.title, self.tags.album,
                     self.tags.artist, self.icon)
+
+        " scrobble lastfm "
+        r = LFM().scrobble(self.tags.artist, self.tags.title)
+        print(r)
+
+    def write_icon(self, source):
+        with open(self.icon, "wb") as outfile:
+            outfile.write(source)
 
     # +----------------------------+
     # | Extract Image from mp3 tag |
     # +----------------------------+
 
     def extract_cover(self):
+
+        """
+        1) get image from tag
+        2) get image from folder (cover.jpg or album.jpg or ???)
+        3) get from lasastfm
+        4) default image
+        """
+
+        tmp_image = '/tmp/xyz.jpg'
+
         if (self.tags.picture):
             data = self.tags[stagger.id3.APIC][0].data
-            self.icon = '/tmp/xyz.jpg'
-            with open(self.icon, "wb") as outfile:
-                outfile.write(data)
+            self.icon = tmp_image
+            self.write_icon(data)
         else:
             self.icon = 'music'
+
+        self.icon = 'music'
+        if (self.icon == 'music'):
+            try:
+                url = self.lfm.get_cover(self.tags.artist,
+                                         self.tags.album)
+                r = requests.get(url)
+                self.icon = tmp_image
+                " TODO md5 of current file - do not download again "
+                print('lastFm cover')
+                self.write_icon(r.content)
+
+            except Exception as e:
+                print(e)
+                pass
 
     # +------------------------------+
     # | Get Object Tag from filepath |
